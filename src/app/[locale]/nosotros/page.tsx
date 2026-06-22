5d18e73ac40c3f029f22adc10b1b29327a0475a3
Oscar Esteban Hernandez Vallin <53105338+oscar-vallin@users.noreply.github.com>
2026-06-22 11:48:05 -0600

    update

--- src/app/[locale]/nosotros/page.tsx
+++ src/app/[locale]/nosotros/page.tsx
@@ -66,7 +66,7 @@ export default async function NosotrosPage({
         imageAlt=""
         mobileObjectPosition="object-[center_25%]"
         imagePosition="md:object-[85%_-120%]"
-        imageClassName="scale-165 xl:scale-125 2xl:scale-100 object-[center_20%]"
+        imageClassName="scale-165 object-[center_20%]"
         solidLeftBand
       />
 
